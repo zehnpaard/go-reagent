300b3483200bc0a7569e61804811bc1795accdbe
zehnpaard <zehnpaard@googlemail.com>
2017-06-25 19:59:39 -0400

    Change structure to support figwheel

--- build.clj
+++
@@ -1,2 +0,0 @@
-(require 'cljs.build.api)
-(cljs.build.api/build "src" {:output-to "out/main.js"})
---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject go-reagent "0.0.1"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.293"]]
+
+  :plugins [[lein-cljsbuild "1.1.4"]
+            [lein-figwheel "0.5.8"]]
+
+  :cljsbuild
+  {:builds
+   {:dev {:source-paths ["src"]
+          :figwheel true
+          :compiler {:output-to "resources/public/js/main.js"
+                     :output-dir "resources/public/js/out/"}}}})
