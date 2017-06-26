8c400ba223621bb538dfd2ddc241b9f6c419880f
zehnpaard <zehnpaard@googlemail.com>
2017-06-25 21:51:46 -0400

    Update BoardIntersection to take a onClick method

--- src/go_reagent/views.cljs
+++ src/go_reagent/views.cljs
@@ -4,15 +4,17 @@
 
 (def GRID_SIZE 40)
 
-(defn BoardIntersection [[row col] color]
+(defn BoardIntersection [[row col] color click-fn]
   (let [style {:top (* row GRID_SIZE)
                :left (* col GRID_SIZE)}
         classes (str "intersection" 
                      (cond (= color :black) " black"
                            (= color :white) " white"
                            :else            ""))]
-    [:div {:className classes :style style}]))
+    [:div {:className classes 
+           :style style 
+           :onClick #(click-fn [row col])}]))
 
 (defn Game []
   [:div
-   [BoardIntersection [2 3] :white]])
+   [BoardIntersection [2 3] :white (fn [[row col]] (js/alert (str row "," col)))]])
