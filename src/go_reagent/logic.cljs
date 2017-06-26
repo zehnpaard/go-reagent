527a893ea1d13d90a9351c30e99464fc262cb858
zehnpaard <zehnpaard@googlemail.com>
2017-06-26 05:37:15 -0400

    Initial split to get-group and count-liberties

--- src/go_reagent/logic.cljs
+++ src/go_reagent/logic.cljs
@@ -42,33 +42,29 @@
         [(inc i) j]]
        (filter #(valid-coord game-state %))))
 
-(defn get-group [move game-state]
+(defn get-group [game-state move]
   (let [color (get-in game-state [:board move])]
     (if-not (= color :empty)
       (loop [visited #{}
-             visited-list []
-             queue [move]
-             liberties 0]
+             queue [move]]
         (cond 
           (empty? queue)
-          {:liberties liberties
-           :stones visited-list}
+          visited
 
           (visited (first queue))
-          (recur visited visited-list (rest queue) liberties)
+          (recur visited (rest queue))
 
           :else
           (let [current   (first queue)
                 neighbors (get-adjacent game-state current)
-                empties   (filter #(= :empty (get-in game-state [:board %]))
-                                  neighbors)
                 sames     (filter #(= color (get-in game-state [:board %]))
                                   neighbors)]
             (recur 
               (conj visited current)
-              (conj visited-list current)
-              (into (rest queue) sames)
-              (+ liberties (count empties)))))))))
+              (into (rest queue) sames))))))))
+
+(defn count-liberties [game-state stones]
+  true)
 
 (defn remove-stone [game-state coord]
   (assoc-in game-state [:board coord] :empty))
@@ -84,20 +80,20 @@
                         (assoc-in [:board move] color)
                         switch-player)
           neighbors (get-adjacent game-state move)
-          n-others  (filter #(and (not= :empty (get-in new-state [:board %]))
+          n-other   (filter #(and (not= :empty (get-in new-state [:board %]))
                                   (not= color (get-in new-state [:board %]))) 
                             neighbors)
-          n-groups  (map #(get-group % new-state) n-others)
-          captured  (filter #(zero? (:liberties %)) n-groups)
-          atari     (some #(= 1 (:liberties %)) n-groups)]
+          n-groups  (map #(get-group new-state %) n-other)
+          captured  (filter #(zero? (count-liberties new-state %)) n-groups)
+          atari     (some #(= 1 (count-liberties new-state %)) n-groups)]
       (if (and (empty? captured)
-               (-> move (get-group new-state) :liberties zero?))
+               (->> move (get-group new-state) (count-liberties new-state) zero?))
         [false
          (-> game-state
            (assoc :in-atari false)
            (assoc :attempted-suicide true))]
         [true
          (-> new-state
-             (remove-stones (->> captured (map :stones) (apply concat)))
+             (remove-stones (apply concat captured))
              (assoc :in-atari atari)
              (assoc :last-move-passed false))]))))
